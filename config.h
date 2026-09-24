72fd86295592de6b346fbb37e8a1a3ae1e1a3c58
Jonas Ornfelt <jonasornfelt@outlook.com>
2026-09-24 07:18:43 +0200

    config.h: use a darker selection background
    #fbf1c7 on #458588 only had a 3.7:1 contrast. #076678 gives 5.8:1 for
    the selected text and 3.9:1 for highlights on it.

--- config.h
+++ config.h
@@ -16,8 +16,8 @@ static const char *colors[SchemeLast][2] = {
     /*     fg         bg       */
     /* the bg of the *Highlight schemes is taken from their base scheme */
     [SchemeNorm] = { "#ebdbb2", "#282828" },
-    [SchemeSel] = { "#fbf1c7", "#458588" },
-    [SchemeSelHighlight] = { "#fabd2f", "#458588" },
+    [SchemeSel] = { "#fbf1c7", "#076678" },
+    [SchemeSelHighlight] = { "#fabd2f", "#076678" },
     [SchemeNormHighlight] = { "#d79921", "#282828" },
     [SchemeOut] = { "#282828", "#8ec07c" },
     [SchemeOutHighlight] = { "#9d0006", "#8ec07c" },
